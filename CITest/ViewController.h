cb38b84a29a54ecb02678f5d39588da44efb8547
bunnyirsa <srtongjitu@163.com>
2017-08-29 14:15:47 +0800

    Initial Commit

---
+++ CITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CITest
+//
+//  Created by 石锐 on 2017/8/29.
+//  Copyright © 2017年 石锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CITest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CITest
+//
+//  Created by 石锐 on 2017/8/29.
+//  Copyright © 2017年 石锐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
